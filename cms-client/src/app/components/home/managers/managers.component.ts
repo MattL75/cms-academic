d9d437e91ab6b614ca9f5dbbd19bad7dfb2d1a1e
Matt Lajoie <mlajoie03@gmail.com>
2018-07-30 22:04:45 -0400

    cleaned manager table + added loading circle

--- cms-client/src/app/components/home/managers/managers.component.ts
+++ cms-client/src/app/components/home/managers/managers.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatSnackBar, MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatSort, MatTableDataSource } from '@angular/material';
 import { SnackbarService } from '../../../services/snackbar.service';
 import { ManagersService } from '../../../services/entity/managers.service';
 import { ManagersDialogComponent } from './managers-dialog/managers-dialog.component';
@@ -13,31 +13,36 @@ import { ConfirmDialogComponent } from '../../utils/confirm-dialog/confirm-dialo
 export class ManagersComponent implements OnInit {
 
     TEST_DATA: Manager[] = [
-        {employeeId: 1, first_name: 'John', last_name: 'Doe', email: 'john.doe@doe.com'},
-        {employeeId: 2, first_name: 'Rob', last_name: 'Ford', email: 'crack@ford.com'},
-        {employeeId: 3, first_name: 'Doug', last_name: 'Ford', email: 'doug@ford.com'},
-        {employeeId: 4, first_name: 'Kebab', last_name: 'Kebabo', email: 'kebabo@kebabo.com'},
-        {employeeId: 5, first_name: 'Martin', last_name: 'Spasov', email: 'nerd1@kebabo.com'},
-        {employeeId: 6, first_name: 'Jesse', last_name: 'Tremblay', email: 'nerd2@kebabo.com'},
-        {employeeId: 7, first_name: 'Manel', last_name: 'Guay-Montserrat', email: 'nerd3@kebabo.com'},
-        {employeeId: 8, first_name: 'Mathieu', last_name: 'Lajoie', email: 'nerd4@kebabo.com'},
-        {employeeId: 9, first_name: 'Richard', last_name: 'Lajoie', email: 'richard@nuance.com'},
-        {employeeId: 10, first_name: 'Alain', last_name: 'Ratier', email: 'ratier@soluteo.com'},
-        {employeeId: 11, first_name: 'Marijane', last_name: 'Moreau-Peterson', email: 'marijane@soluteo.com'},
-        {employeeId: 11, first_name: 'Marijane', last_name: 'Moreau-Peterson', email: 'marijane@soluteo.com'},
-        {employeeId: 11, first_name: 'Marijane', last_name: 'Moreau-Peterson', email: 'marijane@soluteo.com'},
-        {employeeId: 11, first_name: 'Marijane', last_name: 'Moreau-Peterson', email: 'marijane@soluteo.com'},
-        {employeeId: 11, first_name: 'Marijane', last_name: 'Moreau-Peterson', email: 'marijane@soluteo.com'},
-        {employeeId: 11, first_name: 'Marijane', last_name: 'Moreau-Peterson', email: 'marijane@soluteo.com'},
+        {id: 1, first_name: 'John', last_name: 'Doe', email: 'john.doe@doe.com'},
+        {id: 2, first_name: 'Rob', last_name: 'Ford', email: 'crack@ford.com'},
+        {id: 3, first_name: 'Doug', last_name: 'Ford', email: 'doug@ford.com'},
+        {id: 4, first_name: 'Kebab', last_name: 'Kebabo', email: 'kebabo@kebabo.com'},
+        {id: 5, first_name: 'Martin', last_name: 'Spasov', email: 'nerd1@kebabo.com'},
+        {id: 6, first_name: 'Jesse', last_name: 'Tremblay', email: 'nerd2@kebabo.com'},
+        {id: 7, first_name: 'Manel', last_name: 'Guay-Montserrat', email: 'nerd3@kebabo.com'},
+        {id: 8, first_name: 'Mathieu', last_name: 'Lajoie', email: 'nerd4@kebabo.com'},
+        {id: 9, first_name: 'Richard', last_name: 'Lajoie', email: 'richard@nuance.com'},
+        {id: 10, first_name: 'Alain', last_name: 'Ratier', email: 'ratier@soluteo.com'},
+        {id: 11, first_name: 'Marijane', last_name: 'Moreau-Peterson', email: 'marijane@soluteo.com'},
+        {id: 11, first_name: 'Marijane', last_name: 'Moreau-Peterson', email: 'marijane@soluteo.com'},
+        {id: 11, first_name: 'Marijane', last_name: 'Moreau-Peterson', email: 'marijane@soluteo.com'},
+        {id: 11, first_name: 'Marijane', last_name: 'Moreau-Peterson', email: 'marijane@soluteo.com'},
+        {id: 11, first_name: 'Marijane', last_name: 'Moreau-Peterson', email: 'marijane@soluteo.com'},
+        {id: 11, first_name: 'Marijane', last_name: 'Moreau-Peterson', email: 'marijane@soluteo.com'},
     ];
 
-    dataSource = new MatTableDataSource<Manager>(this.TEST_DATA);
-    displayedColumns: string[] = ['employeeId', 'name', 'email', 'actions'];
+    dataSource: MatTableDataSource<Manager>;
+    displayedColumns: string[] = ['id', 'name', 'email', 'actions'];
+    querying = false;
 
-    constructor(private snackbar: SnackbarService, public dialog: MatDialog, private http: ManagersService) {
+    @ViewChild(MatSort) sort: MatSort;
+
+    constructor(private snackbar: SnackbarService, public dialog: MatDialog, private managersService: ManagersService) {
     }
 
     ngOnInit() {
+        this.dataSource = new MatTableDataSource<Manager>();
+        this.dataSource.sort = this.sort;
         this.populate();
     }
 
@@ -55,8 +60,7 @@ export class ManagersComponent implements OnInit {
             // Create Manager, subscribe, then snackbar inside.
             // Backend generates id, then returns here in subscribe, get new item created
             if (result) {
-                this.TEST_DATA.push(result);
-                this.populate();
+                this.dataSource.data.push(result);
                 this.snackbar.open('Manager added.', 'Success!');
             }
         });
@@ -94,15 +98,20 @@ export class ManagersComponent implements OnInit {
 
         dialogRef.afterClosed().subscribe(result => {
             if (result) {
-                // Or just repopulate
-                this.TEST_DATA.splice(this.TEST_DATA.indexOf(element), 1);
-                this.populate();
+                this.dataSource.data.splice(this.TEST_DATA.indexOf(element), 1);
                 this.snackbar.open('Manager deleted.', 'Success!');
             }
         });
     }
 
     private populate(): void {
-        this.dataSource = new MatTableDataSource<Manager>(this.TEST_DATA);
+        this.dataSource.data = this.TEST_DATA;
+
+        // TODO Uncomment when API works
+        // this.querying = true;
+        // this.managersService.getManagers().subscribe(managers => {
+        //     this.dataSource.data = managers;
+        //     this.querying = false;
+        // });
     }
 }
