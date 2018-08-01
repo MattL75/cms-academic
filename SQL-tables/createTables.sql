11bb94146076f1285772a6d3c87958cebd48dc4b
manel-gm <manel.gm@gmail.com>
2018-08-01 13:15:43 -0400

    several small changes
    changes types, field names, added fields, etc.

--- SQL-tables/createTables.sql
+++ SQL-tables/createTables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Department
 (
-  service_type VARCHAR(255) NOT NULL UNIQUE,
+  service_type VARCHAR(255) NOT NULL,
   name VARCHAR(30) NOT NULL UNIQUE,
   id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY 
 );
@@ -17,20 +17,20 @@ CREATE TABLE Contract_Type
   id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY 
 );
 
-CREATE TABLE Deliverables
+CREATE TABLE Deliverable
 (
   due_date DATE NOT NULL,
-  deliverable_number INT(3) NOT NULL,
+  number INT(3) NOT NULL,
   is_final BOOLEAN NOT NULL,
-  name VARCHAR(30) NOT NULL,
+  contract_type VARCHAR(30) NOT NULL,
   id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  FOREIGN KEY (name) REFERENCES Contract_Type(name)
+  FOREIGN KEY (contract_type) REFERENCES Contract_Type(name)
 );
 
 CREATE TABLE Insurance_Plan
 (
-  insurance_type VARCHAR(255) NOT NULL UNIQUE,
-  rate DOUBLE NOT NULL,
+  type VARCHAR(255) NOT NULL UNIQUE,
+  rate INT(2) NOT NULL,
   id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY 
 );
 
@@ -42,103 +42,104 @@ CREATE TABLE Role
 
 CREATE TABLE Province
 (
-  province_name VARCHAR(30) NOT NULL UNIQUE,
+  name VARCHAR(30) NOT NULL UNIQUE,
   id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY 
 );
 
 CREATE TABLE City
 (
-  city_name VARCHAR(30) NOT NULL UNIQUE,
+  name VARCHAR(30) NOT NULL UNIQUE,
   province_name VARCHAR(30) NOT NULL,
   id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-  FOREIGN KEY (province_name) REFERENCES Province(province_name)
+  FOREIGN KEY (province_name) REFERENCES Province(name)
 );
 
 CREATE TABLE Address
 (
   id INT(10) AUTO_INCREMENT PRIMARY KEY,
-  postal_code VARCHAR(6) NOT NULL,
-  street_address VARCHAR(30) NOT NULL,
+  postal_code CHAR(6) NOT NULL,
+  street_address VARCHAR(30) NOT NULL UNIQUE,
   city_name VARCHAR(30) NOT NULL,
   province_name VARCHAR(30) NOT NULL,
-  FOREIGN KEY (city_name) REFERENCES City(city_name),
-  FOREIGN KEY (province_name) REFERENCES Province(province_name)
+  FOREIGN KEY (city_name) REFERENCES City(name),
+  FOREIGN KEY (province_name) REFERENCES Province(name)
 );
 
 CREATE TABLE User
 (
-  id INT(6) NOT NULL AUTO_INCREMENT,
+  id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(30) NOT NULL UNIQUE,
   password VARCHAR(30) NOT NULL,
   is_admin BOOLEAN NOT NULL,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (name) REFERENCES Role(name)
+  role VARCHAR(30) NOT NULL,
+  FOREIGN KEY (role) REFERENCES Role(name)
 );
 
 CREATE TABLE Client
 ( 
-  id INT(6) NOT NULL AUTO_INCREMENT,
+  id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email_domain VARCHAR(255) NOT NULL,
   name VARCHAR(30) NOT NULL,
   province_name VARCHAR(30) NOT NULL,
   user_id INT(6) NOT NULL ,
-  PRIMARY KEY (id),
-  FOREIGN KEY (province_name) REFERENCES Province(province_name),
+  FOREIGN KEY (province_name) REFERENCES Province(name),
   FOREIGN KEY (user_id) REFERENCES User(id)
 );
 
 CREATE TABLE Employee
 (
   id INT(6) NOT NULL AUTO_INCREMENT,
-  last_name VARCHAR(30) NOT NULL,
   first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   department_id INT(6) NOT NULL,
-  insurance_type VARCHAR(255) NOT NULL,
+  insurance_type VARCHAR(255),
   province_name VARCHAR(30) NOT NULL,
   user_id INT(6) NOT NULL,
+  managed_by INT(6) NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (department_id) REFERENCES Department(id),
-  FOREIGN KEY (insurance_type) REFERENCES Insurance_Plan(insurance_type),
-  FOREIGN KEY (province_name) REFERENCES Province(province_name),
-  FOREIGN KEY (user_id) REFERENCES User(id)
+  FOREIGN KEY (insurance_type) REFERENCES Insurance_Plan(type),
+  FOREIGN KEY (province_name) REFERENCES Province(name),
+  FOREIGN KEY (user_id) REFERENCES User(id),
+  FOREIGN KEY (managed_by) REFERENCES Manager(id)
 );
 
 CREATE TABLE works_in
 (
   client_id INT(6) NOT NULL,
-  name VARCHAR(30) NOT NULL,
+  business_line VARCHAR(30) NOT NULL,
   line_id int(6) NOT NULL,
   PRIMARY KEY (client_id, line_id),
   FOREIGN KEY (client_id) REFERENCES Client(id),
-  FOREIGN KEY (name) REFERENCES Business_Line(name),
+  FOREIGN KEY (business_line) REFERENCES Business_Line(name),
   FOREIGN KEY (line_id) REFERENCES Business_Line(id)
 );
 
 CREATE TABLE Contract
 (
-  id INT(6) NOT NULL AUTO_INCREMENT,
-  acv INT(20) NOT NULL,
+  id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  acv DOUBLE NOT NULL,
   start_date DATE NOT NULL,
-  initial_amount INT(10) NOT NULL,
-  client_satisfaction INT(2) NOT NULL,
+  initial_amount DOUBLE NOT NULL,
+  client_satisfaction INT(2),
   recorded_by INT(6) NOT NULL,
   department_id INT(6) NOT NULL,
   client_id INT(6) NOT NULL,
-  line_of_buisness VARCHAR(30) NOT NULL,
+  business_line VARCHAR(30) NOT NULL,
   contract_type VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id),
+  managed_by INT(6) NOT NULL,
   FOREIGN KEY (department_id) REFERENCES Department(id),
   FOREIGN KEY (client_id) REFERENCES Client(id),
-  FOREIGN KEY (line_of_buisness) REFERENCES Business_Line(name),
-  FOREIGN KEY (contract_type) REFERENCES Contract_Type(name)
+  FOREIGN KEY (business_line) REFERENCES Business_Line(name),
+  FOREIGN KEY (contract_type) REFERENCES Contract_Type(name),
+  FOREIGN KEY (managed_by) REFERENCES Manager(id)
 );
 
 CREATE TABLE Manager
 (
   email VARCHAR(30) NOT NULL,
   phone_number VARCHAR(30) NOT NULL,
-  middle_initial VARCHAR(30) NOT NULL,
+  middle_initial CHAR(1),
   id INT(6) NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (id) REFERENCES Employee(id)
@@ -146,7 +147,7 @@ CREATE TABLE Manager
 
 CREATE TABLE Hours
 (
-  hours_worked INT(10) NOT NULL,
+  hours_worked DOUBLE NOT NULL,
   employee_id INT(6) NOT NULL,
   date_worked DATE NOT NULL,
   contract_id INT(6) NOT NULL,
@@ -159,7 +160,7 @@ CREATE TABLE Manages
 (
   contract_id INT(6) NOT NULL,
   manager_id INT(6) NOT NULL,
-  PRIMARY KEY (contract_id, manager_id),
+  PRIMARY KEY (contract_id),
   FOREIGN KEY (contract_id) REFERENCES Contract(id),
   FOREIGN KEY (manager_id) REFERENCES Manager(id)
 );
@@ -168,7 +169,7 @@ CREATE TABLE Supervises
 (
   manager_id INT(6) NOT NULL,
   employee_id INT(6) NOT NULL,
-  PRIMARY KEY (manager_id),
+  PRIMARY KEY (employee_id),
   FOREIGN KEY (manager_id) REFERENCES Manager(id),
   FOREIGN KEY (employee_id) REFERENCES Employee(id)
 );
