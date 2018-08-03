0205f03670c1c862a04e1bf8b7fd89077243cb54
Matt Lajoie <mlajoie03@gmail.com>
2018-08-02 22:28:06 -0400

    added more guards

---
+++ cms-client/src/app/guards/sales.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Role } from '../models/enums/role.enum';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SalesGuard implements CanActivate {
+    constructor(public auth: AuthService, public router: Router) {
+    }
+
+    canActivate(): boolean {
+        if (!this.auth.isAuthenticated()) {
+            this.router.navigate(['auth']);
+            return false;
+        } else if (this.auth.getCurrentUser().role !== Role.SALES_ASSOCIATE) {
+            return false;
+        }
+        return true;
+    }
+}
