044e4cab15106c260a0851d00f59e93cb200a463
Matt Lajoie <mlajoie03@gmail.com>
2018-08-03 23:28:03 -0400

    Added filtering by name for managers department_id

--- cms-client/src/app/components/home/managers/managers-dialog/managers-dialog.component.ts
+++ cms-client/src/app/components/home/managers/managers-dialog/managers-dialog.component.ts
@@ -6,6 +6,9 @@ import { Manager } from '../../../../models/manager.model';
 import { InsuranceType } from '../../../../models/enums/insurance.enum';
 import { Province } from '../../../../models/enums/province.enum';
 import { Role } from '../../../../models/enums/role.enum';
+import { Department } from '../../../../models/department.model';
+import { map, startWith } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'cms-managers-dialog',
@@ -13,8 +16,6 @@ import { Role } from '../../../../models/enums/role.enum';
     styleUrls: ['./managers-dialog.component.scss', '../../home.component.scss']
 })
 export class ManagersDialogComponent implements OnInit {
-
-    // TODO add filtering for the autocomplete fields
 
     managersForm = new FormGroup({
         id: new FormControl(null),
@@ -31,7 +32,8 @@ export class ManagersDialogComponent implements OnInit {
         role: new FormControl(Role.MANAGER),
         is_admin: new FormControl(false),
     });
-    departments: number[];
+    departments: Department[];
+    filteredDepartments: Observable<Department[]>;
     insuranceTypes = Object.keys(InsuranceType);
     provinces = Object.keys(Province);
 
@@ -54,8 +56,14 @@ export class ManagersDialogComponent implements OnInit {
         this.managersForm.controls['password'].setValue(this.data.manager.password);
 
         this.depts.getDepartments().subscribe(depts => {
-            this.departments = depts.map(dept => dept.id);
+            this.departments = depts;
         });
+
+        this.filteredDepartments = this.managersForm.controls['department_id'].valueChanges
+            .pipe(
+                startWith(''),
+                map(value => this.departmentFilter(value))
+            );
     }
 
     public close(): void {
@@ -64,6 +72,25 @@ export class ManagersDialogComponent implements OnInit {
 
     public closeSubmit(): void {
         this.dialogRef.close(this.managersForm.value);
+    }
+
+    // Needed to access variables inside the component
+    getDepartmentLinkFn() {
+        return (val) => this.displayDepartmentFn(val);
+    }
+
+    // Performs the actual operation of mapping id to name
+    displayDepartmentFn(department: number): string | undefined {
+        const result = this.departments.find(x => x.id === department);
+        return result ? result.name : undefined;
+    }
+
+    private departmentFilter(value: string): Department[] {
+        if (typeof value !== 'string') {
+            return null;
+        }
+        const filterValue = value.toLowerCase();
+        return this.departments.filter(option => option.name.toLowerCase().includes(filterValue));
     }
 
 }
