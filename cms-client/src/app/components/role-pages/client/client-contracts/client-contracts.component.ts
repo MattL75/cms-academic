541a650ec719e18b6128555ed5d23c5c19bc4cba
Matt Lajoie <mlajoie03@gmail.com>
2018-08-05 11:06:00 -0400

    added some user-pages

---
+++ cms-client/src/app/components/role-pages/client/client-contracts/client-contracts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cms-client-contracts',
+  templateUrl: './client-contracts.component.html',
+  styleUrls: ['./client-contracts.component.scss']
+})
+export class ClientContractsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
