fa963cf39e181f7da5a633415c9aa0f3237fc88d
Matt Lajoie <mlajoie03@gmail.com>
2018-08-05 10:55:10 -0400

    Removed body from error messages

--- cms-client/src/app/services/entity/sales-associates.service.ts
+++ cms-client/src/app/services/entity/sales-associates.service.ts
@@ -42,10 +42,6 @@ export class SalesAssociatesService {
     private handleError(error: HttpErrorResponse) {
         if (error.error instanceof ErrorEvent) {
             console.error('An error occurred:', error.error.message);
-        } else {
-            console.error(
-                `Backend returned code ${error.status}, ` +
-                `body was: ${error.error}`);
         }
         return throwError('Something bad happened; please try again later.');
     }
