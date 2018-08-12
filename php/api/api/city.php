85e6d32480cd2c5bc89efd7719bb36e097070b86
Jesse Tremblay <jessetrem@gmail.com>
2018-08-11 22:53:20 -0400

    removed ability to filter by province

--- php/api/api/city.php
+++ php/api/api/city.php
@@ -2,16 +2,12 @@
   include "../../lib/api.php";
 
   function get() {
-    if (!isset($_GET["province_name"])) {
-      throw new Exception("requires province name");
-    }
     
-    $cities = QueryBuilder::select("City", ["name"])
-    ->where("province_name = \"{$_GET["province_name"]}\"")
+    $cities = QueryBuilder::select("City", ["name", "province_name"])
     ->execute();
     $filtered = [];
     foreach ($cities as $city) {
-      array_push($filtered, filter(["name"], $city));
+      array_push($filtered, filter(["name", "province_name"], $city));
     }
     
     return json_encode($filtered);
